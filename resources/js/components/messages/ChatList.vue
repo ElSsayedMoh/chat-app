a22c8d06de8efd66f8b82601282d3f0c27742557
feryalyamany <feryalelyamany@gmail.com>
2024-04-12 00:25:03 +0200

    .

--- resources/js/components/messages/ChatList.vue
+++ resources/js/components/messages/ChatList.vue
@@ -11,11 +11,35 @@
                 <div class="input-group">
                     <div class="input-group-text">
                         <div class="icon icon-lg">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                class="feather feather-search"
+                            >
+                                <circle cx="11" cy="11" r="8"></circle>
+                                <line
+                                    x1="21"
+                                    y1="21"
+                                    x2="16.65"
+                                    y2="16.65"
+                                ></line>
+                            </svg>
                         </div>
                     </div>
 
-                    <input type="text" class="form-control form-control-lg ps-0" placeholder="Search messages or users" aria-label="Search for messages or users...">
+                    <input
+                        type="text"
+                        class="form-control form-control-lg ps-0"
+                        placeholder="Search messages or users"
+                        aria-label="Search for messages or users..."
+                    />
                 </div>
             </form>
         </div>
@@ -23,62 +47,110 @@
         <!-- Chats -->
         <div class="card-list" id="chat-list">
             <!-- Card -->
-            <a v-for="conversation in $root.conversations" v-bind:key="conversation.id" v-bind:href="'#' + conversation.id" @click.prevent="setConversation(conversation)" class="card border-0 text-reset">
+            <a
+                v-for="conversation in $root.conversations"
+                v-bind:key="conversation.id"
+                v-bind:href="'#' + conversation.id"
+                @click.prevent="setConversation(conversation)"
+                class="card border-0 text-reset"
+            >
                 <div class="card-body">
                     <div class="row gx-5">
                         <div class="col-auto">
-                            <div class="avatar" :class="{'avatar-online' : conversation.participants[0].isOnline}">
-                                <img v-bind:src="conversation.participants[0].avatar_url" alt="#" class="avatar-img">
+                            <div
+                                class="avatar"
+                                :class="{
+                                    'avatar-online':
+                                        conversation.participants[0].isOnline,
+                                }"
+                            >
+                                <img
+                                    v-bind:src="
+                                        conversation.participants[0].avatar_url
+                                    "
+                                    alt="#"
+                                    class="avatar-img"
+                                />
                             </div>
                         </div>
 
                         <div class="col">
                             <div class="d-flex align-items-center mb-3">
-                                <h5 class="me-auto mb-0">{{conversation.participants[0].name}}</h5>
-                                <span class="text-muted extra-small ms-2">{{$root.moment(conversation.last_message.created_at).fromNow()}}</span>
+                                <h5 class="me-auto mb-0">
+                                    {{ conversation.participants[0].name }}
+                                </h5>
+                                <span class="text-muted extra-small ms-2">{{
+                                    $root
+                                        .moment(
+                                            conversation.last_message.created_at
+                                        )
+                                        .fromNow()
+                                }}</span>
                             </div>
 
                             <div class="d-flex align-items-center">
                                 <div class="line-clamp me-auto"></div>
-                                    {{conversation.last_message.type == 'attachment' ? conversation.last_message.body.file_name : conversation.last_message.body}}
-                                <div v-if="conversation.new_messages" class="badge badge-circle bg-primary ms-5">
+                                {{
+                                    conversation.last_message.type ==
+                                    "attachment"
+                                        ? conversation.last_message.body
+                                              .file_name
+                                        : conversation.last_message.body
+                                }}
+                                <div
+                                    v-if="conversation.new_messages"
+                                    class="badge badge-circle bg-primary ms-5"
+                                >
                                     <span>{{ conversation.new_messages }}</span>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div><!-- .card-body -->
+                </div>
+                <!-- .card-body -->
             </a>
 
             <!-- Card -->
-
         </div>
         <!-- Chats -->
     </div>
 </template>
-
 <script>
-    export default {
-        data(){
-            return {
-            };
+export default {
+    methods: {
+        truncateText(text, max_length) {
+            if (text && text.length > max_length) {
+                return text.slice(0, max_length) + "...";
+            } else {
+                return text;
+            }
         },
-        methods: {
-            setConversation(conversation){
-                this.$root.conversation = conversation;
-                this.$root.markAsRead(conversation);
-            },
+        setConversation(conversation) {
+            this.$root.conversation = conversation;
+            this.$root.markAsRead(conversation);
         },
-        mounted(){
-            fetch('/api/conversations')
-                .then(response => response.json())
-                .then(json => {
-                    for(let i in json.data){
-                        let chat = json.data[i].participants[0].isOnline = false;
+    },
+    mounted() {
+        fetch("/api/conversations")
+            .then((response) => response.json())
+            .then((json) => {
+                for (let i in json.data) {
+                    json.data[i].participants[0].isOnline = false;
+                    console.log(json.data);
+                    if (json.data[i].last_message.type === 'attachment') {
+                        json.data[i].last_message.body.file_name = this.truncateText(
+                            json.data[i].last_message.body.file_name,
+                            20
+                        );
+                    } else {
+                            json.data[i].last_message.body = this.truncateText(
+                            json.data[i].last_message.body,
+                            30
+                        );
                     }
-                    this.$root.conversations = json.data;
-
-                })
-        }
-    }
+                }
+                this.$root.conversations = json.data;
+            });
+    },
+};
 </script>
